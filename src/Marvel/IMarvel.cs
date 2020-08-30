7c217672cc6dccd01ab1c9f26e42ef98c8932c0c
Dario Alonso <dariodotnet@gmail.com>
2020-08-30 21:00:11 +0100

    Get characters json or model

--- src/Marvel/IMarvel.cs
+++ src/Marvel/IMarvel.cs
@@ -1,7 +1,14 @@
 namespace Marvel
 {
+    using Model;
+    using System.Threading;
+    using System.Threading.Tasks;
+
     public interface IMarvel
     {
         IMarvel Initialize(string publicKey, string privateKey, bool bypassCertificate = false);
+
+        Task<string> GetCharacters(CancellationToken token);
+        Task<ApiDataWrapper> GetCharactersDto(CancellationToken token);
     }
 }
