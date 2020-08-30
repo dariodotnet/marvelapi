d9f748dd86374b063e11a5b258b20b53d6e20b1a
Dario Alonso <dariodotnet@gmail.com>
2020-08-30 22:46:38 +0100

    Error constants

---
+++ src/Marvel/Model/ErrorConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace Marvel.Model
+{
+    public static class ErrorConstants
+    {
+        public static class Code
+        {
+            public static string InvalidCredentials = "InvalidCredentials";
+        }
+
+        public static class Message
+        {
+            public static string ApiKeyInvalid = "The passed API key is invalid.";
+            public static string ApiInvalidHash = "That hash, timestamp and key combination is invalid.";
+        }
+    }
+}
