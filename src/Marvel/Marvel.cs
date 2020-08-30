13598fef476e5f2f9263f568c5040f3d9adc0fcd
Dario Alonso <dariodotnet@gmail.com>
2020-08-30 21:03:37 +0100

    Using second iterface

--- src/Marvel/Marvel.cs
+++ src/Marvel/Marvel.cs
@@ -1,5 +1,6 @@
 ﻿namespace Marvel
 {
+    using Model;
     using System;
     using System.Net.Http;
     using System.Threading;
@@ -9,6 +10,7 @@
     {
         private const string BaseUri = "https://gateway.marvel.com/v1/public";
         private IMarvelApi _api;
+        private IMarvelApiDto _apiDto;
 
         public Marvel(string publicKey, string privateKey, bool bypassCertificate = false)
         {
@@ -17,19 +19,21 @@
 
         public IMarvel Initialize(string publicKey, string privateKey, bool bypassCertificate = false) =>
              InitializeApi(publicKey, privateKey, bypassCertificate);
-        
+
         public Task<string> GetCharacters(CancellationToken token) => _api.GetCharacters(token);
+
+        public Task<ApiDataWrapper> GetCharactersDto(CancellationToken token) => _apiDto.GetCharacters(token);
 
         private Marvel InitializeApi(string publicKey, string privateKey, bool bypassCertificate = false)
         {
             HttpClientHandler innerHandler = null;
-            
-            if(bypassCertificate)
+
+            if (bypassCertificate)
                 innerHandler = new HttpClientHandler
                 {
                     ServerCertificateCustomValidationCallback = (message, certificate2, arg3, arg4) => true
                 };
-            
+
             var basicConnection = new HttpLoggingHandler(publicKey, privateKey, innerHandler);
 
             var basicClient = new HttpClient(basicConnection)
@@ -38,6 +42,7 @@
             };
 
             _api = Refit.RestService.For<IMarvelApi>(basicClient);
+            _apiDto = Refit.RestService.For<IMarvelApiDto>(basicClient);
             return this;
         }
     }
