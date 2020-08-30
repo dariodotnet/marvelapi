a6d98ee2c6fc61e8843e06b897b189b25e2a8f21
Dario Alonso <dariodotnet@gmail.com>
2020-08-30 20:23:52 +0100

    Allow response plain json or api dtos

---
+++ src/Marvel/ResponseExtension.cs
@@ -0,0 +1,42 @@
+namespace Marvel
+{
+    using System;
+    using System.IO;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json;
+
+    internal static class ResponseExtension
+    {
+        internal static async Task<string> Resolve(this HttpResponseMessage response)
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                using (var stream = await response.Content.ReadAsStreamAsync())
+                using (var reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+            
+            throw new Exception("Oops! Something went wrong.");
+        }
+
+        internal static async Task<T> Resolve<T>(this HttpResponseMessage response)
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                var serializer = new JsonSerializer();
+                
+                using (var stream = await response.Content.ReadAsStreamAsync())
+                using (var reader = new StreamReader(stream))
+                using (var json = new JsonTextReader(reader))
+                {
+                    return serializer.Deserialize<T>(json);
+                }
+            }
+            
+            throw new Exception("Oops! Something went wrong.");
+        }
+    }
+}
