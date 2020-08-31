95de17695fcd81549b6d22c23d7157b79327437e
Dario Alonso <dariodotnet@gmail.com>
2020-08-31 09:16:32 +0100

    Unify throw exception

--- src/Marvel/ResponseExtension.cs
+++ src/Marvel/ResponseExtension.cs
@@ -20,6 +20,28 @@ namespace Marvel
                 }
             }
 
+            throw await ThrowException(response);
+        }
+
+        internal static async Task<T> Resolve<T>(this HttpResponseMessage response)
+        {
+            var serializer = new JsonSerializer();
+
+            if (response.IsSuccessStatusCode)
+            {
+                using (var stream = await response.Content.ReadAsStreamAsync())
+                using (var reader = new StreamReader(stream))
+                using (var json = new JsonTextReader(reader))
+                {
+                    return serializer.Deserialize<T>(json);
+                }
+            }
+
+            throw await ThrowException(response);
+        }
+
+        internal static async Task<Exception> ThrowException(HttpResponseMessage response)
+        {
             var serializer = new JsonSerializer();
 
             using (var stream = await response.Content.ReadAsStreamAsync())
@@ -29,23 +51,6 @@ namespace Marvel
                 var error = serializer.Deserialize<MarvelError>(json);
                 if (error != null)
                     throw error;
-            }
-
-            throw new Exception("Oops! Something went wrong.");
-        }
-
-        internal static async Task<T> Resolve<T>(this HttpResponseMessage response)
-        {
-            if (response.IsSuccessStatusCode)
-            {
-                var serializer = new JsonSerializer();
-
-                using (var stream = await response.Content.ReadAsStreamAsync())
-                using (var reader = new StreamReader(stream))
-                using (var json = new JsonTextReader(reader))
-                {
-                    return serializer.Deserialize<T>(json);
-                }
             }
 
             throw new Exception("Oops! Something went wrong.");
