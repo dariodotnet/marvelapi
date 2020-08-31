ea2ec2e40c3c6cd7f0a35689b7c5a51c3c7addb7
Dario Alonso <dariodotnet@gmail.com>
2020-08-31 11:10:33 +0100

    Normalize model

---
+++ src/Marvel/Model/MarvelResponse.cs
@@ -0,0 +1,176 @@
+﻿namespace Marvel.Model
+{
+    using Newtonsoft.Json;
+    using System;
+
+    public class MarvelResponse<T>
+    {
+        [JsonProperty("code")]
+        public int Code { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("copyright")]
+        public string Copyright { get; set; }
+
+        [JsonProperty("attributionText")]
+        public string AttributionText { get; set; }
+
+        [JsonProperty("attributionHTML")]
+        public string AttributionHTML { get; set; }
+
+        [JsonProperty("etag")]
+        public string Etag { get; set; }
+
+        [JsonProperty("data")]
+        public Container<T> Container { get; set; }
+    }
+
+    public class Container<T>
+    {
+        [JsonProperty("offset")]
+        public int Offset { get; set; }
+
+        [JsonProperty("limit")]
+        public int Limit { get; set; }
+
+        [JsonProperty("total")]
+        public int Total { get; set; }
+
+        [JsonProperty("count")]
+        public int Count { get; set; }
+
+        [JsonProperty("results")]
+        public T[] Results { get; set; }
+    }
+
+    public class Character
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("modified")]
+        public DateTime Modified { get; set; }
+
+        [JsonProperty("thumbnail")]
+        public Thumbnail Thumbnail { get; set; }
+
+        [JsonProperty("resourceURI")]
+        public string ResourceUri { get; set; }
+
+        [JsonProperty("comics")]
+        public Comics Comics { get; set; }
+
+        [JsonProperty("series")]
+        public Series Series { get; set; }
+
+        [JsonProperty("stories")]
+        public Stories Stories { get; set; }
+
+        [JsonProperty("events")]
+        public Events Events { get; set; }
+
+        [JsonProperty("urls")]
+        public Url[] Urls { get; set; }
+    }
+
+    public class Thumbnail
+    {
+        [JsonProperty("path")]
+        public string Path { get; set; }
+
+        [JsonProperty("extension")]
+        public string Extension { get; set; }
+
+        [JsonIgnore]
+        public string ImageUrl => $"{Path}{Extension}";
+    }
+
+    public class Comics
+    {
+        [JsonProperty("available")]
+        public int Available { get; set; }
+
+        [JsonProperty("collectionURI")]
+        public string CollectionUri { get; set; }
+
+        [JsonProperty("items")]
+        public Item[] Items { get; set; }
+
+        [JsonProperty("returned")]
+        public int Returned { get; set; }
+    }
+
+    public class Series
+    {
+        [JsonProperty("available")]
+        public int Available { get; set; }
+
+        [JsonProperty("collectionURI")]
+        public string CollectionUri { get; set; }
+
+        [JsonProperty("items")]
+        public Item[] Items { get; set; }
+
+        [JsonProperty("returned")]
+        public int Returned { get; set; }
+    }
+
+    public class Item
+    {
+        [JsonProperty("resourceURI")]
+        public string ResourceUri { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+
+    public class Stories
+    {
+        [JsonProperty("available")]
+        public int Available { get; set; }
+
+        [JsonProperty("collectionURI")]
+        public string CollectionUri { get; set; }
+
+        [JsonProperty("items")]
+        public Item[] Items { get; set; }
+
+        [JsonProperty("returned")]
+        public int Returned { get; set; }
+    }
+
+    public class Events
+    {
+        [JsonProperty("available")]
+        public int Available { get; set; }
+
+        [JsonProperty("collectionURI")]
+        public string CollectionUri { get; set; }
+
+        [JsonProperty("items")]
+        public Item[] Items { get; set; }
+
+        [JsonProperty("returned")]
+        public int Returned { get; set; }
+    }
+
+    public class Url
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("url")]
+        public string UrlPath { get; set; }
+    }
+}
